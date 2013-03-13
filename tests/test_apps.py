3e7a31273de61cf54cd322372df7ad6d05634c8d
Michael <michael@lionvpn.info>
2013-03-13 15:16:45 +0800

    some test case

--- tests/test_apps.py
+++ tests/test_apps.py
@@ -1,16 +1,20 @@
 
 import json
-from tsuru.libs import mock
-from tsuru.libs import nose
 from nose.tools import with_setup, eq_
 import unittest
+from tsuru.libs import mock
 from tsuru.configdb import ConfigDb
 from tsuru.cmds.apps import AppManager
-import os
+import os, sys
 
 DBNAME = os.path.join(os.path.dirname(__file__), "temp", "test.db")
 
-class AppsTestCase(unittest.TestCase):
+ELoginFirst = 'error> Please login first!'
+
+TestAuthHeader = {'Authorization': 'token'}
+
+# with_setup can not works for unittest.TestCase
+class TestAppsTestCase(unittest.TestCase):
 
     def setUp(self):
         self.dbn = DBNAME
@@ -20,27 +24,45 @@ class AppsTestCase(unittest.TestCase):
         if os.path.exists(self.dbn):
             os.remove(self.dbn)        
 
+    # now @with_setup(loggedin) can not working?
+    # nose can not working with unittest.Testcase or it's subclass
     def loggedin(self):
         self.cfgdb.add_user("test", "test@gmail.com", "token", True)        
     
     @mock.patch("requests.get")
-    @with_setup(setup=loggedin)
-    def test_list(self, get):
+    def test_ListAppsWithLogin(self, get):
         t = self.cfgdb.get_default_target()
         eq_(t['name'], 'local')
         eq_(t['url'], 'http://127.0.0.1:8080')
-        self.cfgdb.add_user("test", "test@gmail.com", "token", True)
+        self.loggedin()
         am = AppManager("target", self.dbn)
         am.list()
-        get.assert_called_with("target/apps", headers={'Authorization':'token'})
+        get.assert_called_with("target/apps", headers=TestAuthHeader)
+
+    @mock.patch("requests.get")
+    def test_ListAppsWithoutLogin(self, get):
+        am = AppManager("target", self.dbn)        
+        am.list()
+        get.not_called() 
+        output = sys.stdout.getvalue().strip() # because stdout is an StringIO instance
+        self.assertEquals(output, ELoginFirst)
+
+    @mock.patch("requests.get")
+    def test_GetAppWithLogin(self, get):
+        self.loggedin()
+        am = AppManager("target", self.dbn)        
+        am.get("myapp")
+        get.assert_called_with("target/apps/myapp", headers=TestAuthHeader)
+
+    @mock.patch("requests.get")
+    def test_GetAppWithoutLogin(self, get):
+        am = AppManager("target", self.dbn)        
+        am.get("myapp")
+        get.not_called() 
+        output = sys.stdout.getvalue().strip() # because stdout is an StringIO instance
+        self.assertEquals(output, ELoginFirst)
 
     '''
-    @mock.patch("requests.get")
-    def test_list_apps(self, get):
-        cl = client.Client("target")
-        cl.apps.list()
-        get.assert_called_with("target/apps")
-
     @mock.patch("requests.get")
     def test_get_app(self, get):
         cl = client.Client("target")
--- tsuru/cmds/users.py
+++ tsuru/cmds/users.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
 
-import sys
-sys.path.append("..")
-
 import os
 import json
 import requests
